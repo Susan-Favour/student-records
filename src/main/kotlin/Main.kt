1de7a9842131e2185034fe36a130dbcd57302ed5
susan <susanfavourodongo@gmail.com>
2024-02-07 22:32:14 +0300

    add records

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+fun main(args: Array<String>) {
+    val studentName1 = "Latifah Pierre"
+    var studentAge1 = "24"
+    var studentContact1 = "086443468"
+    var studentWeightInKg1 = "76"
+    val kenyan1 = "true"
+
+    println("studentName1: Latifah Pierre")
+    println("studentAge1: 24")
+    println("studentContact1:086443468")
+    println("studentWeightInKg1: 76")
+    println("kenyan2: true")
+
+    val studentName2 = "Stacy Martin"
+    var studentAge2 = "26"
+    var studentContact2 = "098765432"
+    var studentWeightInKg2 = "93"
+    val kenyan2 = "false"
+
+    println("studentName2: Stacy Martin" )
+    println("studentAge2: 26")
+    println("studentContact2: 098765432")
+    println("studentWeightInKg1: 93")
+    println("kenyan2: false")
+
+
+
+
+
+
+
+}
